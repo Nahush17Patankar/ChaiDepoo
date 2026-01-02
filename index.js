53a749f7437d3ac7c29f352fcfbcb5814627b142
nahush p <nahushpatankar17@gmail.com>
2026-01-02 11:00:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express= require('express');
+const app = express();
+
+const port=3000;
+
+app.get('/',(req,res) => {
+    res.send("Hi");
+
+})
+
+app.get('/twitter', (req,res) => {
+    res.send("Welcome to twitter");
+
+})
+
+app.get('/login',(req,res) => {
+    res.send('<h1>Lofin in nahush code </h1>');
+
+})
+
+app.get('/youtube', (req,res) => {
+    res.send("<h2>Welcome to Chai</h2>");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`App listening on port ${port}`);
+
+})
